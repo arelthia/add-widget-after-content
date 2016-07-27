14015835dd5caeae78cd31c8115e3c07cadae109
Pintop <ap.gwhere@gmail.com>
2016-07-26 23:04:58 -0500

    Fix spelling errors

--- partials/awac-options-display.php
+++ partials/awac-options-display.php
@@ -37,11 +37,11 @@
                 <div class="postbox">
                     <div class="inside">
                         <p><?php $url2 = 'https://https://pintopsolutions.com/contact/';
-                            $link2     = sprintf( __( 'Need help? Or have an idea how this plugin can be made better. Reach out <a href=%s>on our website?</a>', 'link-timestamp' ), esc_url( $url2 ) );
+                            $link2     = sprintf( __( 'Need help? Or have an idea how this plugin can be made better. Reach out <a href=%s>on our website?</a>', $this->plugin_name ), esc_url( $url2 ) );
                             echo $link2; ?></p>
 
                         <p><?php $url4 = 'https://wordpress.org/support/view/plugin-reviews/add-widget-after-content?filter=5#postform';
-                            $link4        = sprintf( __( 'We invite you to <a href=%s>leave an honest review.</a>', 'link-timestamp' ), esc_url( $url4 ) );
+                            $link4        = sprintf( __( 'We invite you to <a href=%s>leave an honest review.</a>', $this->plugin_name ), esc_url( $url4 ) );
                             echo $link4; ?></p>
 
                     </div>
@@ -59,11 +59,11 @@
                             <form method="post" action="options.php">
                                 <?php
                                 settings_fields( $active_tab );
-                                $section = ('basic' == $active_tab) ? 'awac-options' : $active_tab; 
+                                $section = ('awac_basic' == $active_tab) ? 'awac-options' : $active_tab;
                                 do_settings_sections( $section );
                                 if ( ! class_exists( 'AWAC_comments' ) ) {
                                     $url1 = 'https://pintopsolutions.com/downloads/awac-comments/';
-                                    $link1       = sprintf( __( '<strong>Show After Comments</strong> - Now you can add AWAC after comments. <a href=%s>Learn More</a>', 'link-timestamp' ), esc_url( $url1 ) );
+                                    $link1       = sprintf( __( '<strong>Show After Comments</strong> - Now you can add AWAC after comments. <a href=%s>Learn More</a>', $this->plugin_name ), esc_url( $url1 ) );
                                     echo $link1;
                                 }
                                 submit_button();
